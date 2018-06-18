ead9eadf2432d9d06d1a1f8fda949d98a1eef7be
Victor Yukio <victorkikio@gmail.com>
2018-06-18 09:02:17 -0300

    exercicios

--- exercicio 1.js
+++
@@ -1,3 +0,0 @@
-let idade = prompt ("Quantos anos você tem ?")
-let emprestimo = ("Quanto dinheiro você quer de emprestimo ?")
-let salario = prompt ("Quanto você ganha por mês")
---
+++ exercicio3/escada.js
@@ -0,0 +1,11 @@
+let tamanho = Number (prompt("Qual o tamanho da escadinha"));
+let contador = 0;
+let escadinha = "";
+
+while(contador < tamanho){
+    escadinha = escadinha + "#"
+console.log(escadinha);
+contador++;
+}
+
+

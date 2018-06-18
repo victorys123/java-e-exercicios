55f152f6a7176a68907d3b7f49416cb2aa7a0035
Victor Yukio <victorkikio@gmail.com>
2018-06-18 12:03:40 -0300

    atualização

---
+++ exercicio1/exer1.js
@@ -0,0 +1,36 @@
+/*let idade = Number (prompt ("Qual a sua idade ?"));
+let emprestimo = Number (prompt ("Quanto você precisa de emprestimo ?"));
+let  salario = Number (prompt("Quanto você recebe por mês ?"));
+
+
+if (isNaN(idade)){
+    alert("ERRO: VALOR INVÁLIDO");
+
+}
+else if (idade <= 21 && >= 56) {
+    alert("Reprovado");
+}
+else if (idade >=22 && <= 55 ) {
+    alert("Aprovado");
+}
+else {
+    alert("erro");
+}
+*/
+
+
+
+let idade = Number(prompt("Digite a sua idade"));
+let valor = Number(prompt("Digite o valor que você precisa emprestado?"));
+let salario = Number(prompt("Quanto você recebe por mês?"));
+
+
+
+if (isNaN) (22>idade && idade<55
+     && 1000>valor && valor>15*salario) {
+    alert("NÃO ACEITO");
+}
+
+else {
+    alert("ACEITO")
+}
